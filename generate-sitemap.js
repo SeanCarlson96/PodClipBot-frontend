85581d299a05c974fe1f49dbac085360bdf685c1
SeanCarlson96 <carlsonseanr@gmail.com>
2023-06-22 09:36:23 -0500

    added sitemap generator

---
+++ generate-sitemap.js
@@ -0,0 +1,17 @@
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { createWriteStream } = require('fs');
+const { routes } = require('./routes-list');
+
+let sitemap = new SitemapStream({ hostname: 'https://podclipbot.com/' });
+
+routes.forEach(route => {
+  sitemap.write({ url: route });
+});
+
+sitemap.end();
+
+// This will generate a sitemap.xml file in your project root
+streamToPromise(sitemap).then((data) => {
+  createWriteStream("sitemap.xml").write(data.toString());
+})
+.catch((error) => console.error(error));
---
+++ routes-list.js
@@ -0,0 +1,12 @@
+const routes = [
+    '/',
+    '/tool',
+    '/login',
+    '/subscriptions',
+    '/registration',
+    '/privacy-policy',
+    '/terms-of-use',
+    '/contact',
+  ];
+
+module.exports = { routes };
